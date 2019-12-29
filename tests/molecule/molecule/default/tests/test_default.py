278a7ef3078683a3dbc744f85ce4568c996d00b1
Tomoyuki Sakurai <y@trombik.org>
2019-12-29 23:40:54 +0900

    wip: add molecule test

--- tests/molecule/molecule/default/tests/test_default.py
+++ tests/molecule/molecule/default/tests/test_default.py
@@ -8,15 +8,7 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 def get_service_name(host):
-    if host.system_info.distribution == 'freebsd':
-        return 'openssh'
-    if host.system_info.distribution == 'openbsd':
-        return 'sshd'
-    elif host.system_info.distribution == 'ubuntu':
-        return 'sshd'
-    elif host.system_info.distribution == 'centos':
-        return 'sshd'
-    raise NameError('Unknown distribution')
+    return 'argus'
 
 
 def get_ansible_vars(host):
@@ -39,13 +31,6 @@ def get_ping_target(host):
               )
 
 
-def read_remote_file(host, filename):
-    f = host.file(filename)
-    assert f.exists
-    assert f.content is not None
-    return f.content.decode('utf-8')
-
-
 def is_docker(host):
     ansible_facts = get_ansible_facts(host)
     if 'ansible_virtualization_type' in ansible_facts:
@@ -54,23 +39,8 @@ def is_docker(host):
     return False
 
 
-def read_digest(host, filename):
-    uri = "ansible://client1?ansible_inventory=%s" \
-            % os.environ['MOLECULE_INVENTORY_FILE']
-    client1 = host.get_host(uri)
-    return read_remote_file(client1, filename)
-
-
 def get_listen_ports(host):
-    if host.system_info.distribution == 'freebsd':
-        return [22, 10022]
-    if host.system_info.distribution == 'openbsd':
-        return [22, 10022]
-    elif host.system_info.distribution == 'ubuntu':
-        return [22, 10022]
-    elif host.system_info.distribution == 'centos':
-        return [22]
-    raise NameError('Unknown distribution')
+    return [561]
 
 
 def test_hosts_file(host):
@@ -102,30 +72,12 @@ def test_icmp_from_server(host):
 def test_service(host):
     s = host.service(get_service_name(host))
 
-    # XXX in docker, host.service() does not work
-    if not is_docker(host):
-        assert s.is_running
-        assert s.is_enabled
+    assert s.is_running
+    assert s.is_enabled
 
 
 def test_port(host):
     ports = get_listen_ports(host)
 
     for p in ports:
-        assert host.socket("tcp://:::%d" % p).is_listening
-
-
-def test_find_digest1_on_client(host):
-    ansible_vars = get_ansible_vars(host)
-    if ansible_vars['inventory_hostname'] == 'client1':
-        f = host.file('/tmp/digest1')
-
-        assert f.exists
-
-
-def test_find_digest2_on_client(host):
-    ansible_vars = get_ansible_vars(host)
-    if ansible_vars['inventory_hostname'] == 'client1':
-        f = host.file('/tmp/digest2')
-
-        assert f.exists
+        assert host.socket("tcp://127.0.0.1:%d" % p).is_listening
